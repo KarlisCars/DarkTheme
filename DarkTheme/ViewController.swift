fbe9c428166397437b8b1ca60ea0ba6eaea252a8
Karlis Cars <karlis.cars@gmail.com>
2019-10-24 16:47:45 +0300

    Adding a new icon

--- DarkTheme/ViewController.swift
+++ DarkTheme/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DarkButton: UIButton!
+    var isOn = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func darkButtonTapped(_ sender: Any) {
+        if isOn {
+            self.isOn = true
+            self.DarkButton.setTitle("Dark Theme On", for: .normal)
+            self.DarkButton.setTitleColor(UIColor.white, for: .normal)
+            self.view.backgroundColor = UIColor.black
+            
+        }else{
+            self.isOn = false
+            self.DarkButton.setTitle("Dark Theme On", for: .normal)
+            self.DarkButton.setTitleColor(UIColor.white, for: .normal)
+            self.view.backgroundColor = UIColor.white}
+    }
+    
 }
 
